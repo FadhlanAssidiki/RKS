89a2f967fb642dfc61522715ced596f53fb204de
FadhlanAssidiki <alan.assidiki@gmail.com>
2021-02-01 20:02:11 +0700

    rks

---
+++ app/src/main/java/com/fadhlan/spotify/dyhohaw.java
@@ -0,0 +1,27 @@
+package com.fadhlan.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class dyhohaw extends AppCompatActivity {
+    private ListView listViewdyhohaw;
+    private ArrayAdapter<String> arrayAdapter;
+    private String[] programmingLang = {
+            "List Lagu", "List Lagu", "List Lagu", "List Lagu","List Lagu", "List Lagu",
+            "List Lagu", "List Lagu", "List Lagu", "List Lagu", "List Lagu", "List Lagu","List Lagu",
+            "List Lagu", "List Lagu"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dyhohaw);
+
+        listViewdyhohaw=(ListView) findViewById(R.id.listviewdyhohaw);
+        arrayAdapter=new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,programmingLang);
+        listViewdyhohaw.setAdapter(arrayAdapter);
+    }
+}
---
+++ app/src/main/java/com/fadhlan/spotify/favorit.java
@@ -0,0 +1,14 @@
+package com.fadhlan.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class favorit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorit);
+    }
+}
